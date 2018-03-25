dff16d40b024d0c87398776d776c0ac19447c93d
HellBrick <hellbrick@outlook.com>
2018-03-25 22:57:26 +0300

    Added IEnumerable<T>.ForEach()

---
+++ Utils/Enumerable.ForEach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+	public static partial class EnumerableExtensions
+	{
+		public static void ForEach<TItem>( this IEnumerable<TItem> sequence, Action<TItem> action )
+			=> sequence.ForEach( action, ( capturedAction, item ) => capturedAction( item ) );
+
+		public static void ForEach<TItem, TCapture>( this IEnumerable<TItem> sequence, TCapture capture, Action<TCapture, TItem> action )
+		{
+			foreach ( TItem item in sequence )
+				action( capture, item );
+		}
+	}
+}
