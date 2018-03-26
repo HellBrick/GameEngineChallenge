69824854c7a85583a0b97696a6c27056260b2ccc
HellBrick <hellbrick@outlook.com>
2018-03-26 22:15:08 +0300

    Added AddRequisiteAction

---
+++ GameEngineChallenge.Test/AddRequisiteActionTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using GameEngineChallenge.Actions;
+using GameEngineChallenge.Services;
+using Utils;
+using Xunit;
+
+namespace GameEngineChallenge.Test
+{
+	public class AddRequisiteActionTest
+	{
+		[Fact]
+		public void RequisitesWithTheSameIdDontStack()
+		{
+			BoringRequisite requisite = new BoringRequisite();
+			Hero hero = new Hero( requisite );
+			HeroService heroService = new HeroService( hero.AsArray() );
+			GameContext context = new GameContext( heroService );
+
+			new AddRequisiteAction( hero, requisite ).Execute( context );
+
+			hero.Requisites.Count.Should().Be( 1 );
+			hero.Requisites.Should().HaveElementAt( 0, requisite );
+		}
+
+		private class BoringRequisite : IRequisite
+		{
+			public RequisiteId Id => new RequisiteId( nameof( BoringRequisite ) );
+		}
+	}
+}
