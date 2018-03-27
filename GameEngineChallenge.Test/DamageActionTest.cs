6059c251da9413e177588d2ad7a83ab3177b2ce3
HellBrick <hellbrick@outlook.com>
2018-03-28 00:27:55 +0300

    Added DamageAction

---
+++ GameEngineChallenge.Test/DamageActionTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using GameEngineChallenge.Actions;
+using GameEngineChallenge.Services;
+using Utils;
+using Xunit;
+
+namespace GameEngineChallenge.Test
+{
+	public class DamageActionTest
+	{
+		[Theory]
+		[InlineData( 0, 0, 0 )]
+		[InlineData( 42, 0, 42 )]
+		[InlineData( 15, 15, 0 )]
+		[InlineData( 13, 12, 1 )]
+		[InlineData( 50, 9999, 0 )]
+		public void DamageMathChecksOut( uint initialHp, uint damage, uint expectedHp )
+		{
+			Hero hero = new Hero( team: default, new HitPoints( initialHp ) );
+			HeroService heroService = new HeroService( hero.AsArray() );
+			GameContext context = new GameContext( heroService, new InputService(), new TimeService(), new SpaceService() );
+
+			new DamageAction( hero, new HitPoints( damage ) ).Execute( context );
+
+			hero.HP.Should().Be( new HitPoints( expectedHp ) );
+		}
+	}
+}
---
+++ GameEngineChallenge/Actions/DamageAction.cs
@@ -0,0 +1,21 @@
+﻿namespace GameEngineChallenge.Actions
+{
+	public class DamageAction : IAction
+	{
+		public DamageAction( Hero target, HitPoints damage )
+		{
+			_target = target;
+			_damage = damage;
+		}
+
+		private readonly Hero _target;
+		private readonly HitPoints _damage;
+
+		public void Execute( GameContext context )
+		{
+			_target.HP = new HitPoints( GetNewHpValue() );
+
+			uint GetNewHpValue() => _target.HP < _damage ? 0 : _target.HP.Value - _damage.Value;
+		}
+	}
+}
