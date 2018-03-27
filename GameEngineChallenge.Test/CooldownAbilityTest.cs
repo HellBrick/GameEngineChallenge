c13764e29a92bf2f1ff7513195404375cc278b9b
HellBrick <hellbrick@outlook.com>
2018-03-28 02:01:02 +0300

    Added IActiveAbility.WithCooldown()

---
+++ GameEngineChallenge.Test/CooldownAbilityTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using GameEngineChallenge.Abilities;
+using Xunit;
+
+namespace GameEngineChallenge.Test
+{
+	public class CooldownAbilityTest
+	{
+		private static readonly TimeSpan _cooldown = TimeSpan.FromSeconds( 1 );
+
+		[Fact]
+		public void CooldownIsEnabledAfterFirstInvocation()
+		{
+			CooldownAbility cooldownAbility
+				= AbilityBuilder
+				.CreateActiveAbility( phase: default, ( h, c ) => { } )
+				.WithCooldown( _cooldown );
+
+			cooldownAbility.GetActions( default, default ).Should().HaveCount( 1 );
+			cooldownAbility.TimeLeft.Should().Be( _cooldown );
+		}
+
+		[Fact]
+		public void CooldownIsNotEnabledIfAbilityDoesNotEmitAnyActions()
+		{
+			CooldownAbility cooldownAbility
+				= AbilityBuilder
+				.CreateActiveAbility( phase: default, ( h, c ) => Array.Empty<IAction>() )
+				.WithCooldown( _cooldown );
+
+			cooldownAbility.GetActions( default, default ).Should().HaveCount( 0 );
+			cooldownAbility.TimeLeft.Should().Be( TimeSpan.Zero );
+		}
+
+		[Fact]
+		public void NoActionsAreEmittedIfCooldownIsEnabled()
+		{
+			CooldownAbility cooldownAbility
+				= AbilityBuilder
+				.CreateActiveAbility( phase: default, ( h, c ) => { } )
+				.WithCooldown( _cooldown );
+
+			cooldownAbility.TimeLeft = TimeSpan.FromSeconds( 1 );
+			cooldownAbility.GetActions( default, default ).Should().HaveCount( 0 );
+		}
+	}
+}
---
+++ GameEngineChallenge/Abilities/CooldownAbility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameEngineChallenge.Abilities
+{
+	public static partial class ActiveAbilityExtensions
+	{
+		public static CooldownAbility WithCooldown( this IActiveAbility activeAbility, TimeSpan cooldown )
+			=> new CooldownAbility( activeAbility, cooldown );
+	}
+
+	public class CooldownAbility : IActiveAbility, ITimer
+	{
+		public CooldownAbility( IActiveAbility innerAbility, TimeSpan cooldown )
+		{
+			InnerAbility = innerAbility;
+			_cooldown = cooldown;
+
+			TimeLeft = TimeSpan.Zero;
+		}
+
+		private readonly TimeSpan _cooldown;
+
+		public IActiveAbility InnerAbility { get; }
+		public TimeSpan TimeLeft { get; set; }
+
+		public RequisiteId Id => InnerAbility.Id;
+		public TickPhase Phase => InnerAbility.Phase;
+
+		public IReadOnlyCollection<IAction> GetActions( Hero abilityOwner, GameContext context )
+		{
+			if ( TimeLeft > TimeSpan.Zero )
+				return Array.Empty<IAction>();
+
+			IReadOnlyCollection<IAction> actions = InnerAbility.GetActions( abilityOwner, context );
+			if ( actions.Count > 0 )
+				TimeLeft = _cooldown;
+
+			return actions;
+		}
+	}
+}
