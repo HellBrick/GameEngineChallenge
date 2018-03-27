783f4860ddd51e14791988fe822a885ba61b4c19
HellBrick <hellbrick@outlook.com>
2018-03-28 01:07:11 +0300

    Added AutoAttackAbility

--- GameEngineChallenge/CommonTickPhases.cs
+++ GameEngineChallenge/CommonTickPhases.cs
@@ -3,5 +3,6 @@
 	public static class CommonTickPhases
 	{
 		public static TickPhase TimeAdjustments { get; } = new TickPhase( 0 );
+		public static TickPhase OffensiveAbilities { get; } = new TickPhase( TimeAdjustments.Order + 1 );
 	}
 }
