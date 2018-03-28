7adf70712202ac0417526a78c757677d5701d194
HellBrick <hellbrick@outlook.com>
2018-03-28 23:44:49 +0300

    Added a heroless overload of CreateAction()

--- GameEngineChallenge.Test/AbilityBuilder.CreateAction.cs
+++ GameEngineChallenge.Test/AbilityBuilder.CreateAction.cs
@@ -4,6 +4,9 @@ namespace GameEngineChallenge.Test
 {
 	public static partial class AbilityBuilder
 	{
+		public static IAction CreateAction( Action<GameContext> actionLambda )
+			=> new LambdaAction( default, ( _, c ) => actionLambda( c ) );
+
 		public static IAction CreateAction( Hero hero, Action<Hero, GameContext> actionLambda )
 			=> new LambdaAction( hero, actionLambda );
 	}
