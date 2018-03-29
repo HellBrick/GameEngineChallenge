3fe23fb1eaa195f41f8c8f09017105f19ed018d6
HellBrick <hellbrick@webkontrol.ru>
2018-03-29 15:06:50 +0300

    Added a targeter that targets all allies in a specified radius

---
+++ GameEngineChallenge/Abilities/AttackTargeters/AlliesInRadiusTargeter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Utils.SpaceTime;
+
+namespace GameEngineChallenge.Abilities.AttackTargeters
+{
+	public class AlliesInRadiusTargeter : ITargeter
+	{
+		public AlliesInRadiusTargeter( Distance radius ) => _radius = radius;
+
+		private readonly Distance _radius;
+
+		public IEnumerable<Hero> EnumerateTargets( Hero actor, GameContext context )
+		{
+			Position actorPosition = context.SpaceService.GetHeroPosition( actor );
+			return
+				context.HeroService.Heroes
+				.Where( hero => hero.Team == actor.Team && hero != actor )
+				.Select( hero => (Hero: hero, Distance: Distance.Between( actorPosition, context.SpaceService.GetHeroPosition( hero ) )) )
+				.Where( pair => pair.Distance <= _radius )
+				.Select( pair => pair.Hero );
+		}
+	}
+}
