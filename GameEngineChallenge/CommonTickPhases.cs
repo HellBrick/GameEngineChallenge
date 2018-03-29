3910ef44bf5a37dcdaf18e0f2b9b1bd05133db91
HellBrick <hellbrick@webkontrol.ru>
2018-03-29 13:55:17 +0300

    Inserted a passive ability phase between time adjustments and offensive abilities

--- GameEngineChallenge/CommonTickPhases.cs
+++ GameEngineChallenge/CommonTickPhases.cs
@@ -4,6 +4,7 @@
 	{
 		public static TickPhase DeathCheck { get; } = new TickPhase( 0 );
 		public static TickPhase TimeAdjustments { get; } = new TickPhase( DeathCheck.Order + 1 );
-		public static TickPhase OffensiveAbilities { get; } = new TickPhase( TimeAdjustments.Order + 1 );
+		public static TickPhase PassiveAbilities { get; } = new TickPhase( TimeAdjustments.Order + 1 );
+		public static TickPhase OffensiveAbilities { get; } = new TickPhase( PassiveAbilities.Order + 1 );
 	}
 }
