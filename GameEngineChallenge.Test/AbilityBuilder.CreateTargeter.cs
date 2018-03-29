a597b91c26454125be230cdb755926a170428242
HellBrick <hellbrick@webkontrol.ru>
2018-03-29 14:27:57 +0300

    Added AbilityBuilder.CreateTargeter

---
+++ GameEngineChallenge.Test/AbilityBuilder.CreateTargeter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GameEngineChallenge.Abilities;
+using Utils;
+
+namespace GameEngineChallenge.Test
+{
+	public static partial class AbilityBuilder
+	{
+		public static ITargeter CreateTargeter( Func<Hero, GameContext, Hero> targetSelector )
+			=> new LambdaTargeter( ( h, c ) => targetSelector( h, c ).AsArray() );
+
+		public static ITargeter CreateTargeter( Func<Hero, GameContext, IEnumerable<Hero>> targetSelector )
+			=> new LambdaTargeter( targetSelector );
+
+		private class LambdaTargeter : ITargeter
+		{
+			public LambdaTargeter( Func<Hero, GameContext, IEnumerable<Hero>> targetSelector ) => _targetSelector = targetSelector;
+
+			private readonly Func<Hero, GameContext, IEnumerable<Hero>> _targetSelector;
+
+			public IEnumerable<Hero> EnumerateTargets( Hero actor, GameContext context ) => _targetSelector( actor, context );
+		}
+	}
+}
